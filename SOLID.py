5cea5f34446f5094883728fcc098e556302392ef
artix77 <pratamaputra12397@gmail.com>
2026-05-20 16:02:05 +0700

    tugas SOLID.py

---
+++ SOLID.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+class Hewan(ABC):
+    def __init__(self, nama):
+        self.nama = nama
+
+    def nama_hewan(self):
+        print(f"Nama hewan: {self.nama}")
+
+class HewanBerjalan(ABC):
+    @abstractmethod
+    def berjalan(self):
+        pass
+
+class HewanTerbang(ABC):
+    @abstractmethod
+    def terbang(self):
+        pass
+
+class kandang:
+    def __init__(self):
+        self.hewan_list = []
+
+    def tambah_hewan(self, hewan):
+        self.hewan_list.append(hewan)
+
+    def bersihkan_kandang(self):
+        print("Kandang dibersihkan.")
+
+class kebun_binatang:
+    def __init__(self):
+        self.kandang = kandang()
+
+    def rawat_semua_hewan(self):
+        for hewan in self.kandang.hewan_list:
+            hewan.makan()
