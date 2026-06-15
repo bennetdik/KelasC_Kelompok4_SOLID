c4958b63ed8a53703c84fd0566a93ab36d0e48c5
Akbar <buattugas474@gmail.com>
2026-06-15 14:31:54 +0700

    Menambahkan fitur jurnal

--- SOLID.py
+++ SOLID.py
@@ -1,17 +1,16 @@
-class Jurnal(Koleksi):
-    def __init__(self, kode, judul, tahun_terbit, penerbit,
-                 bidang_studi, impact_factor):
+from koleksi_perpustakaan import KoleksiPerpustakaan
 
-        super().__init__(kode, judul, tahun_terbit, penerbit)
+class Majalah(KoleksiPerpustakaan):
+    def __init__(self, kode, judul, tahun, penerbit, edisi):
+        super().__init__(kode, judul, tahun, penerbit)
+        self.edisi = edisi
 
-        self.bidang_studi = bidang_studi
-        self.impact_factor = impact_factor
-
-    def tampilkan_data(self):
-        print(f"Jenis          : Jurnal")
-        print(f"Kode Koleksi   : {self.kode}")
-        print(f"Judul          : {self.judul}")
-        print(f"Tahun Terbit   : {self.tahun_terbit}")
-        print(f"Penerbit       : {self.penerbit}")
-        print(f"Impact Factor  : {self.impact_factor}")
-        print(f"Bidang Studi   : {self.bidang_studi}")
+    def tampilkan_detail(self, nomor):
+        print(f"Koleksi Nomor {nomor} :")
+        print(f"Jenis         : Majalah")
+        print(f"Kode Koleksi  : {self.kode_koleksi}")
+        print(f"Judul         : {self.judul}")
+        print(f"Tahun Terbit  : {self.tahun_terbit}")
+        print(f"Penerbit      : {self.penerbit}")
+        print(f"Edisi         : {self.edisi}")
+        
